e71dc9483c0f0a07f4fd18c1d5658d73d6e45f6f
Matheus Braga C1325543 <romanobraga.m@gmail.com>
2022-12-26 11:57:08 -0300

    feat: create and configurate tags module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { TagModule } from '@app/tag/tag.module';
 
+
+// Module means that this class could be injected into other classes
+// If you have other modules, you can import them here, and connect them to other modules
 @Module({
-  imports: [],
+  imports: [TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
